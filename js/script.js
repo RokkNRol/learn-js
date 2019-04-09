ca359664f701c1a0177ee177c9461d5cf20a909c
RockNRoll <himera80@mail.ru>
2019-04-09 10:19:21 +0300

    test while()

--- js/script.js
+++ js/script.js
@@ -1,69 +1,120 @@
 'use strict';
 
-button.onclick = function () {
-   text.value += "in onclick";
-   img.add += "in onclick";
-   text.value += "!focus";
-   text.value += "out onclick";
-   reset.style.backgroundColor = "red";
-   img.style.width = "550px";
-   img.style.height = "100px";
-   button.style.backgroundColor = "#fff";
-   img.style.backgroundColor = "red";
-
-};
-reset.onclick = function () {
-   reset.style.backgroundColor = "#fff";
-   button.style.backgroundColor = "blue";
-   img.style.backgroundColor = "blue";
-};
-
-
-/*end script button*/
-
-/*start script Ul > li*/
-
-var treeUl = document.getElementsByTagName('ul')[0];
-var treeLis = treeUl.getElementsByTagName('li');
-for (var i = 0; i < treeLis.length; i++) {
-   var li = treeLis[i];
-   var span = document.createElement('span');
-   li.insertBefore(span, li.firstChild);
-   span.appendChild(span.nextSibling);
-}
-
-treeUl.onclick = function (event) {
-   var target = event.target;
-   if (target.tagName != 'SPAN') {
-      return;
-   }
-
-   var childrenContainer = target.parentNode.getElementsByTagName('ul')[0];
-   if (!childrenContainer) return;
-   childrenContainer.hidden = !childrenContainer.hidden;
-};
-// end
-
-let persone = {
-   name: "Jone",
-   age: 25,
-   maried: false
-}
-
-console.log(persone.name);
-console.log(persone.name);
-
-let money = +prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "");
-let cashVar = +prompt("Введите обязательную статью расходов в этом месяце", "");
-let hauMach = +prompt("Во сколько обойдется", "");
+//button.onclick = function () {
+//   text.value += "in onclick";
+//   img.add += "in onclick";
+//   text.value += "!focus";
+//   text.value += "out onclick";
+//   reset.style.backgroundColor = "red";
+//   img.style.width = "550px";
+//   img.style.height = "100px";
+//   button.style.backgroundColor = "#fff";
+//   img.style.backgroundColor = "red";
+//
+//};
+//reset.onclick = function () {
+//   reset.style.backgroundColor = "#fff";
+//   button.style.backgroundColor = "blue";
+//   img.style.backgroundColor = "blue";
+//};
+//
+//
+///*end script button*/
+//
+///*start script Ul > li*/
+//
+//var treeUl = document.getElementsByTagName('ul')[0];
+//var treeLis = treeUl.getElementsByTagName('li');
+//for (var i = 0; i < treeLis.length; i++) {
+//   var li = treeLis[i];
+//   var span = document.createElement('span');
+//   li.insertBefore(span, li.firstChild);
+//   span.appendChild(span.nextSibling);
+//}
+//
+//treeUl.onclick = function (event) {
+//   var target = event.target;
+//   if (target.tagName != 'SPAN') {
+//      return;
+//   }
+//
+//   var childrenContainer = target.parentNode.getElementsByTagName('ul')[0];
+//   if (!childrenContainer) return;
+//   childrenContainer.hidden = !childrenContainer.hidden;
+//};
+//// end
+//
+//let persone = {
+//   name: "Jone",
+//   age: 25,
+//   maried: false
+//};
+//
+//console.log(persone.name);
+//console.log(persone.name);
+let money = +prompt("Ваш бюджет на месяц?", ""),
+   time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
    cash: money,
    timeData: time,
-   expenses: {
-      cashVar: hauMach
-   }
+   expenses: {},
+   optionalExpenses: {},
+   income: [],
+   savings: false
+};
+for (var i = 0; i < 2; i++) {
+   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+      b = prompt("Во сколько обойдется", "");
 
+   if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+      console.log("done");
+      appData.expenses[a] = b;
+   } else {
+      i--;
+      continue;
+   }
+};
+//let i = 0;
+//while (i < 2) {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//      b = prompt("Во сколько обойдется", "");
+//
+//   if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+//      console.log("done");
+//      appData.expenses[a] = b;
+//      i++;
+//   } else {
+//      i--;
+//      continue;
+//   }
+//};
+//let i = 0;
+//do {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//      b = prompt("Во сколько обойдется", "");
+//
+//   if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+//      console.log("done");
+//      appData.expenses[a] = b;
+//      i++;
+//   } else {
+//      i--;
+//      continue;
+//   }
+//}
+//while (i < 2);
+
+appData.moneyPerDay = appData.cash / 30;
+alert("ежедневный бюджет: " + appData.moneyPerDay);
+if (appData.moneyPerDay < 100) {
+   console.log("Минимальный уровень достатка");
+   alert("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+   console.log("Средний уровень достатка");
+   alert("Средний уровень достатка");
+} else if (appData.moneyPerDay) {
+   console.log("Высокий уровень достатка");
+   alert("Высокий уровень достатка");
 }
-alert(appData.expenses.cashVar);
+// end script
